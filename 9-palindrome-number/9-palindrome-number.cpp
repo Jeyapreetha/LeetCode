3c9fe9dee67febcd8ef98eae77b362f2761cb0fb
Jeyapreetha <84381895+Jeyapreetha@users.noreply.github.com>
2022-08-31 12:42:04 +0530

    Time: 15 ms (81.43%), Space: 5.9 MB (31.55%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0)
+            return false;
+        
+        long  x1 = 0,y = x;
+        
+        while(x)
+        {
+            //rem = x % 10;
+            x1 = x1 * 10 + x % 10; 
+            x = x / 10;
+        }
+        
+        if(y == x1)
+            return true;
+        return false;
+    }
+};
